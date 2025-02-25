a87a727ef7d9250db67c8633a1aad52f92a8660e
minseochh02 <minseochh02@gmail.com>
2025-02-25 16:57:46 +0900

    testing sign up

--- app/(user)/mydashboard/page.tsx
+++ app/(user)/mydashboard/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import { Loader2 } from "lucide-react";
-
+import { MyDashboardContent } from "./MyDashboardContent";
 export default function MyLinksPage() {
 	return (
 		<Suspense
@@ -9,6 +9,8 @@ export default function MyLinksPage() {
 					<Loader2 className="animate-spin h-6 w-6 text-gray-600" />
 				</div>
 			}
-		></Suspense>
+		>
+			<MyDashboardContent />
+		</Suspense>
 	);
 }
