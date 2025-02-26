36cd52b3a9ce3fec32833b41285286bd3af3b32b
minseochh02 <minseochh02@gmail.com>
2025-02-26 13:44:25 +0900

    success page update

--- app/(auth)/kakao/add/success/SuccessAddContent.tsx
+++ app/(auth)/kakao/add/success/SuccessAddContent.tsx
@@ -1,17 +1,46 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { createClient } from "@/utils/supabase/client";
+import { UserData } from "@/utils/types/data";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function KakaoSuccessAddContent() {
 	const searchParams = useSearchParams();
 	const user_id = searchParams.get("user_id");
 	const router = useRouter();
+	const supabase = createClient();
+	const [subscription, setSubscription] = useState<UserData | null>(null);
+
+	useEffect(() => {
+		const fetchSubscriptionInfo = async () => {
+			const { data: user, error } = await supabase
+				.from("users")
+				.select("*")
+				.eq("id", user_id)
+				.single();
+			if (error) {
+				console.error("Error fetching user:", error);
+				return;
+			}
+			const { data: subscription, error: subscriptionError } = await supabase
+				.from("subscriptions")
+				.select("*")
+				.eq("user_id", user_id)
+				.single();
+			if (subscriptionError) {
+				// TODO: handle error
+				return;
+			}
+			setSubscription(subscription);
+		};
+		fetchSubscriptionInfo();
+	}, []);
 
 	return (
 		<div>
-			<h1>Successfully subscribed to TODO: pass subscription info</h1>
+			<h1>Successfully subscribed to {subscription?.name}</h1>
 		</div>
 	);
 }
