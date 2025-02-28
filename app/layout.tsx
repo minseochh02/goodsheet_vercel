1720aabaaeb9a14612688007acc8b8c036b93180
minseochh02 <minseochh02@gmail.com>
2025-02-28 11:59:22 +0900

    added company info

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,18 +51,25 @@ export default function RootLayout({
 					{children}
 					<Toaster />
 					<footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-						<p>
-							Powered by{" "}
-							<a
-								href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-								target="_blank"
-								className="font-bold hover:underline"
-								rel="noreferrer"
-							>
-								Supabase
-							</a>
-						</p>
-						<ThemeSwitcher />
+						<div className="flex flex-col gap-2">
+							<p>
+								<a
+									href="https://www.quus.cloud/"
+									target="_blank"
+									className="font-bold hover:underline"
+									rel="noreferrer"
+								>
+									(주)쿠스 | QUUS Inc.
+								</a>
+							</p>
+							<p>Email: quus.aispace@gmail.com | Tel: 010-7523-5071</p>
+							<p>
+								Address: 배곧테크노밸리 609호, 경기도 시흥시 서울대학로 59-69
+							</p>
+							<p>
+								&copy; {new Date().getFullYear()} QUUS Inc. All rights reserved.
+							</p>
+						</div>
 					</footer>
 				</ThemeProvider>
 			</body>
