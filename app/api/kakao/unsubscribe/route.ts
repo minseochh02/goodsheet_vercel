c4d1d5f07601abe3dc095797b60a03592499b479
minseochh02 <minseochh02@gmail.com>
2025-02-28 14:35:14 +0900

    testing kakao auth + unsubscribe

--- app/api/kakao/unsubscribe/route.ts
+++ app/api/kakao/unsubscribe/route.ts
@@ -4,7 +4,7 @@ export async function GET(request: NextRequest) {
     const user_id = request.nextUrl.searchParams.get("user_id");
     
     // make call to fastapi backend
-    const response = await fetch("https://minseochh02-goodsheet.hf.space/kakao/authorize-unsubscribe?user_id=" + user_id, {
+    const response = await fetch("https://minseochh02-goodsheet.hf.space/authorize-unsubscribe?user_id=" + user_id, {
         method: "POST",
         body: JSON.stringify({ user_id }),
     });
